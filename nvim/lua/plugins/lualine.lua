bb9f91b2abe806acb17038315ab4967d83be4aac
Jaco Kok <kokjaco2@gmail.com>
2024-10-18 16:08:42 +0200

    bufferline rounded fixes

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,10 +5,35 @@ return {
 		require("lualine").setup({
 			options = {
 				theme = "catppuccin",
-				component_separators = { left = "•", right = "•" }, --  ·•
-				section_separators = { left = "", right = "" }, --  
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
 				globalstatus = true,
+			},
+			sections = {
+				lualine_a = {
+					{ "mode", separator = { left = "" } },
+				},
+				lualine_y = {
+					{ "progress", padding = { left = 0, right = 0 } },
+					{ "location", padding = { left = 0, right = 0 } },
+				},
+				lualine_z = {
+					{ "os.date('%R')", icon = " ", separator = { right = "" } },
+				},
 			},
 		})
 	end,
 }
+
+-- sections = {
+--   lualine_a = [{
+--     __unkeyed-1 = "mode";
+--     separator.left = "";
+--   }];
+--   lualine_y = [ "progress" "location" ];
+--   lualine_z = [{
+--     __unkeyed-1 = ''" " .. os.date("%R")'';
+--     separator.right = "";
+--     # padding = 2;
+--   }];
+-- };
