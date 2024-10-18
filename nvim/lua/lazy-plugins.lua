d8e158ad2e16ce690df4cb0ed206c182661ab15a
Jaco Kok <kokjaco2@gmail.com>
2024-10-18 15:54:07 +0200

    lazy config

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -1,21 +1,4 @@
+local lazy_config = require("config.lazy")
 require("lazy").setup({
 	{ import = "plugins" },
-}, {
-	ui = {
-		icons = vim.g.have_nerd_font and {} or {
-			cmd = "⌘",
-			config = "🛠",
-			event = "📅",
-			ft = "📂",
-			init = "⚙",
-			keys = "🗝",
-			plugin = "🔌",
-			runtime = "💻",
-			require = "🌙",
-			source = "📄",
-			start = "🚀",
-			task = "📌",
-			lazy = "💤 ",
-		},
-	},
-})
+}, lazy_config)
