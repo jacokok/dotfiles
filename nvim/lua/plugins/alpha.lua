699014c32c997cdd3dabb9197b3238d65b54078b
Jaco Kok <kokjaco2@gmail.com>
2024-11-26 11:09:26 +0200

    Removed barbar for now

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -44,33 +44,4 @@ return {
 		}
 		alpha.setup(config)
 	end,
-	-- config = function()
-	-- 	local alpha = require("alpha")
-	-- 	local dashboard = require("alpha.themes.theta")
-	--
-	-- 	dashboard.section.header.val = {
-	--
-	-- 		"        ::::    ::: :::     ::: :::::::::::   :::   ::: ",
-	-- 		"       :+:+:   :+: :+:     :+:     :+:      :+:+: :+:+: ",
-	-- 		"      :+:+:+  +:+ +:+     +:+     +:+     +:+ +:+:+ +:+ ",
-	-- 		"     +#+ +:+ +#+ +#+     +:+     +#+     +#+  +:+  +#+  ",
-	-- 		"    +#+  +#+#+#  +#+   +#+      +#+     +#+       +#+   ",
-	-- 		"   #+#   #+#+#   #+#+#+#       #+#     #+#       #+#    ",
-	-- 		"  ###    ####     ###     ########### ###       ###     ",
-	-- 	}
-	--
-	-- 	-- dashboard.section.buttons.val = {
-	-- 	-- dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-	-- 	-- dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-	-- 	-- dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-	-- 	-- dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-	-- 	-- dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-	-- 	-- dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
-	-- 	-- }
-	--
-	-- 	-- Send config to alpha
-	-- 	alpha.setup(dashboard.config)
-	--
-	-- 	-- vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-	-- end,
 }
--- nvim/lua/plugins/barbar.lua
+++ nvim/lua/plugins/barbar.lua
@@ -1,14 +1,15 @@
-return {
-	{
-		"romgrk/barbar.nvim",
-		dependencies = {
-			"lewis6991/gitsigns.nvim",
-			"nvim-tree/nvim-web-devicons",
-		},
-		init = function()
-			vim.g.barbar_auto_setup = false
-		end,
-		opts = {},
-		version = "^1.0.0", -- optional: only update when a new 1.x version is released
-	},
-}
+return {}
+-- return {
+-- 	{
+-- 		"romgrk/barbar.nvim",
+-- 		dependencies = {
+-- 			"lewis6991/gitsigns.nvim",
+-- 			"nvim-tree/nvim-web-devicons",
+-- 		},
+-- 		init = function()
+-- 			vim.g.barbar_auto_setup = false
+-- 		end,
+-- 		opts = {},
+-- 		version = "^1.0.0", -- optional: only update when a new 1.x version is released
+-- 	},
+-- }
