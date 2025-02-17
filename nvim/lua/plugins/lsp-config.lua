c8bc4ec64d10b638da415522e4b211cdb85bf83c
Jaco Kok <kokjaco2@gmail.com>
2025-02-17 20:54:42 +0200

    New snacks implemented

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -16,16 +16,6 @@ return {
 						mode = mode or "n"
 						vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 					end
-					map("gd", require("fzf-lua").lsp_definitions, "[G]oto [D]efinition")
-					map("gr", require("fzf-lua").lsp_references, "[G]oto [R]eferences")
-					map("gI", require("fzf-lua").lsp_implementations, "[G]oto [I]mplementation")
-
-					-- TODO: Make all def shortcuts start with g
-					map("<leader>D", require("fzf-lua").lsp_typedefs, "Type [D]efinition")
-
-					map("<leader>ds", require("fzf-lua").lsp_document_symbols, "[D]ocument [S]ymbols")
-
-					map("<leader>ws", require("fzf-lua").lsp_live_workspace_symbols, "[W]orkspace [S]ymbols")
 
 					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
