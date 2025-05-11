02b903fe61b15bd62cbd91ed9216f1a1452fcb58
Jaco Kok <kokjaco2@gmail.com>
2025-05-11 09:13:35 +0200

    Added types

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -2,6 +2,8 @@ return {
 	"saghen/blink.cmp",
 	dependencies = "rafamadriz/friendly-snippets",
 	version = "*",
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
 	opts = {
 		keymap = { preset = "default" },
 
