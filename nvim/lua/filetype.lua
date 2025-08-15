e0d34347d449d9fa9b8a0f777aaae6b69162e225
Jaco Kok <kokjaco2@gmail.com>
2025-08-16 00:54:06 +0200

    Now using chez

--- nvim/lua/filetype.lua
+++
@@ -1,6 +0,0 @@
-vim.filetype.add({
-	filename = {
-		["docker-compose.yml"] = "yaml.docker-compose",
-		["docker-compose.yaml"] = "yaml.docker-compose",
-	},
-})
--- nvim/lua/lazy-plugins.lua
+++
@@ -1,4 +0,0 @@
-local lazy_config = require("config.lazy")
-require("lazy").setup({
-	{ import = "plugins" },
-}, lazy_config)
